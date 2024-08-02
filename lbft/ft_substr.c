4b1df938d8b9d5c4a159069118460b2e3034dd68
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2024-08-02 19:59:52 +0100

    good luck

---
+++ lbft/ft_substr.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+char *ft_substr(char const *s, unsigned int start, size_t len) {
+        size_t i;
+        char *str;
+        i = 0;
+        str = malloc(sizeof(char) * len + 1);
+        if(!str)
+            return NULL;
+        while(len > i) {
+            str[i] = s[start + i];
+            i++;
+        }
+        str[i] = '\0';
+        return str;
+}	
