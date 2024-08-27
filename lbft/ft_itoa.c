a55bcd0aa92839eba701f490023c1ff97a9be5e0
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2024-08-27 23:29:17 +0100

    Done

---
+++ lbft/ft_itoa.c
@@ -0,0 +1,58 @@
+#include "libft.h"
+#include <stdlib.h>
+
+static int NumLen(int n) {
+    int count;
+
+    count = 0;
+    if (n == 0) {
+        return 1; 
+    }
+    if (n < 0) {
+        n = -n;
+        count++;
+    }
+    while (n > 0) {
+        n /= 10;
+        count++;
+    }
+    return (count);
+}
+
+char *ft_itoa(int n) {
+    int sign;
+    char *str;
+    int len;
+
+    len = NumLen(n);
+    str = malloc(sizeof(char) * (len + 1));
+
+    if (!str) {
+        return NULL;
+    }
+
+    sign = 0;
+    if (n < 0) {
+        sign = 1;
+        n = -n;
+    }
+
+	str[len] = '\0';
+
+    if (n == 0) {
+        str[0] = '0';
+    }
+    else {
+        while (n > 0) {
+            str[--len] = (n % 10) + '0';
+            n /= 10;
+        }
+    }
+
+    if (sign) {
+        str[0] = '-';
+    }
+
+
+    return (str);
+}
