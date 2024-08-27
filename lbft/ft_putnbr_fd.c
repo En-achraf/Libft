acebf0d6ed86f823f35d70b62e9345b1aeb3d270
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2024-08-28 00:13:50 +0100

    Done

--- lbft/ft_putnbr_fd.c
+++ lbft/ft_putnbr_fd.c
@@ -1,21 +1,22 @@
-void ft_putchar_fd(char c, int fd) {
-    write(fd, &c, 1);
-}
+#include "libft.h"
 
 void ft_putnbr_fd(int n, int fd) {
     if(n == -2147483648) {
         write(fd, "-2147483648", 11);
     }
-    else if(n < 0) {
-        ft_putchar_fd('-', fd);
-        n *= -n;
-        ft_putnbr_fd(n, fd); 
+    else if (n >= 0 && n <= 9)
+    {
+        ft_putchar_fd(n + 48, fd);
     }
-    else if(n > 9) {
-        ft_putnbr_fd((n / 10), fd);
-        ft_putnbr_fd((n % 10), fd);
+    else if (n < 0)
+    {
+        write(1, "-", 1);
+        n = -n;
+        ft_putnbr_fd(n, fd);
     }
-    else{
-        ft_putchar_fd((n + 48), fd);
+    else if (n > 9)
+    {
+        ft_putnbr_fd(n / 10, fd);
+        ft_putnbr_fd(n % 10, fd);
     }
 }
