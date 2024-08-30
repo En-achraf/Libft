a7cd06347a7c4cf30b3a7acef299c892db6406cd
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2024-08-30 22:33:51 +0100

    loading..

--- lbft/libft.h
+++ lbft/libft.h
@@ -6,12 +6,13 @@
 
 void ft_putnbr_fd(int n, int fd);
 void ft_putchar_fd(char c, int fd);
+void ft_putstr_fd(char *s, int fd);
 char *ft_substr(char const *s, unsigned int start, size_t len);
 char *ft_strtrim(char const *s1, char const *set);
 char *ft_strjoin(char const *s1, char const *s2);
 char *ft_strdup(const char *s1);
 char *ft_strchr(const char *s, int c);
-int ft_strlen(char *str);
+size_t	ft_strlen(const char *s);
 int ft_atoi(const char *str);
 int ft_isdigit(int c);
 int ft_isalpha(int c);
