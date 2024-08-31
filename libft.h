1bffe0cc0841de8d80b68edbabbb57385b11dd41
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2024-08-31 18:31:49 +0100

    loading...

--- libft.h
+++ libft.h
@@ -22,5 +22,6 @@ char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void ft_putendl_fd(char *s, int fd);
 void ft_striteri(char *s, void (*f)(unsigned int, char*));
 void *calloc(size_t nitems, size_t size);
+char * strdup( const char *s);
 
 #endif
