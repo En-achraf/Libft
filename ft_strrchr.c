61f51d4a6ff95398ec830a9c556465b506701e71
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2024-09-04 00:22:29 +0100

    Done

---
+++ ft_strrchr.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+char *ft_strrchr(const char *s, int c) {
+	int i;
+	char *X;
+
+	i = 0;
+	X = NULL;
+	while(s[i]) {
+			if(s[i] == (char)c) 
+				X = (char*)&s[i];
+		i++;
+	}
+
+	if(c == '\0')
+		return (char*)&s[i];
+
+	return X;
+}
