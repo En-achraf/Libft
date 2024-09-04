a94ace63313a05e1b0c67391812e960ae42228b3
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2024-09-04 14:16:58 +0100

    Done

--- ft_memset.c
+++ ft_memset.c
@@ -1,13 +1,11 @@
 #include "libft.h"
 
 void *ft_memset(void *s, int c, size_t n) {
-	size_t i;
-	unsigned char *ptr
-	unsigned char X 
+	unsigned char *ptr;
+	unsigned char X;
 
 	ptr = (unsigned char *)s;
 	X = (unsigned char)c;
-	i = 0;
 	while(n > 0) {
 		*ptr++ = X;
 		n--;
