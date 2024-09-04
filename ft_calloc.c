add052c7f7579e520e5391e16f92f63a0766ae3f
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2024-09-04 13:59:25 +0100

    Done

--- ft_calloc.c
+++ ft_calloc.c
@@ -2,18 +2,13 @@
 
 void *calloc(size_t nitems, size_t size) {
 	void *res;
-	size_t i;
 
 	res = malloc(size * nitems);
 
 	if(!res)
 		return NULL;
 
-	i = 0;
-	while(i < (size * nitems)) {
-		((unsigned char*)res)[i] = 0;
-		i++;
-	}
-
+	ft_bzero(res, size * nitems);
+	
 	return res;
 }
