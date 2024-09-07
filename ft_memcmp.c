b868d8e66ce75814023615a3b8af497ab83178f9
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2024-09-07 13:12:29 +0100

    !Done
    its not the last commit

---
+++ ft_memcmp.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+int ft_memcmp(const void *s1, const void *s2, size_t n) {
+
+    unsigned const char *str1;
+    unsigned const char *str2;
+    size_t i;
+
+    str1 = (unsigned const char *)s1;
+    str2 = (unsigned const char *)s2;
+    i = 0;
+    while(n--) {
+        if(str1[i] != str2[i])
+            return str1[i] - str2[i]; 
+        i++;
+    }
+
+    return 0;
+}
--- libft.h
+++ libft.h
@@ -33,5 +33,6 @@ void ft_bzero(void *s, size_t n);
 void *ft_memcpy(void *dest, const void *src, size_t n);
 void *ft_memmove(void *dest, const void *src, size_t n);
 void *ft_memchr(const void *s, int c, size_t n);
+int ft_memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
