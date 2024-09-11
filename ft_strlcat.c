a410686a3a47f1b3549c9d7ec5d738d28f279d0b
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2024-09-11 07:48:27 +0100

    Loading...

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -2,15 +2,13 @@
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
-	size_t	i;
 	size_t	j;
 	size_t	dest_len;
 	size_t	src_len;
 
-	i = 0;
-	while (dest[i] && i < size)
-		i++;
-	dest_len = i;
+	dest_len = 0;
+	while (dest[dest_len] && dest_len < size)
+		dest_len++;
 	src_len = ft_strlen(src);
 	j = 0;
 	if (dest_len < size)
