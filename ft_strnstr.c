65eaf020925f7bea912a329273e3f40709d068d2
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2024-09-11 22:55:56 +0100

    Done

---
+++ ft_strnstr.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t big_str;
+	size_t part_str;
+
+	if (needle[0] == '\0')
+		return ((char *)haystack);
+
+	big_str = 0;
+	while (haystack[big_str] && len > big_str)
+	{
+		part_str = 0;
+		while (needle[part_str])
+		{
+			if (haystack[big_str + part_str] != needle[part_str])
+				break ;
+			part_str++;
+		}
+
+		if (needle[part_str] == '\0')
+			return ((char *)&haystack[big_str]);
+		big_str++;
+	}
+	return (0);
+}
--- libft.h
+++ libft.h
@@ -36,5 +36,6 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 
 #endif
