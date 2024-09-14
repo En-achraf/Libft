5f59cfc1f319b6909cb79774f4c716bb01d1cf4c
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2024-09-14 12:49:46 +0100

    Done

---
+++ ft_lstclear.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list *ptr;
+	t_list *next;
+
+	ptr = *lst;
+	while (ptr)
+	{
+		next = ptr->next;
+		del(ptr->content);
+		free(ptr);
+		ptr = next;
+	}
+	*lst = NULL;
+}
