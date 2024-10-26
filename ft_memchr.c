545a7ec5f3b4fc099651faf51f3771ad324703ad
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2024-10-26 21:18:22 +0100

    finish

--- ft_memchr.c
+++ ft_memchr.c
@@ -2,15 +2,13 @@
 
 void *ft_memchr(const void *s, int c, size_t n) {
     unsigned char *str;
-    size_t i;
 
-    i = 0;
     str = (unsigned char *)s;
-    while (n > i) {
+    while (n > 0) {
         if(*str == (unsigned char)c)
             return (void *)str;
         str++;
-        i++;
+        n--;
     }
     return NULL;
 }
