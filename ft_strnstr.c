50add46acea7572b4ad59570fdb1a71303a9141c
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2024-10-27 09:57:00 +0100

    finish

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,25 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acennadi <acennadi@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/27 08:56:14 by acennadi          #+#    #+#             */
+/*   Updated: 2024/10/27 09:55:29 by acennadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
-{
-	size_t	big_str;
-	size_t	part_str;
-
-	if (needle[0] == '\0')
-		return ((char *)haystack);
-	big_str = 0;
-	while (haystack[big_str] && len > big_str)
-	{
-		part_str = 0;
-		while (needle[part_str])
-		{
-			if (haystack[big_str + part_str] != needle[part_str])
-				break ;
-			part_str++;
-		}
-		if (needle[part_str] == '\0')
-			return ((char *)&haystack[big_str]);
-		big_str++;
-	}
-	return (0);
+char *ft_strnstr(const char *haystack, const char *needle, size_t len) {
+    size_t (j), (i);
+  
+    if (needle[0] == '\0')
+        return ((char *)haystack);
+    i = 0;
+    while(haystack[i] && i < len) {
+        j = 0;
+        while(needle[j] != '\0' && (i + j) < len) {
+            if(needle[j] != haystack[i + j])
+                break;
+            j++;
+        }
+        if(needle[j] == '\0')
+            return ((char *)&haystack[i]);
+        i++;
+    }
+    return (NULL);
 }
