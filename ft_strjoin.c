d760f1bb8ee6e251ce78a482d9ee50cf0c8659ae
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2024-10-28 10:11:07 +0100

    finish

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: acennadi <acennadi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 17:23:03 by acennadi          #+#    #+#             */
-/*   Updated: 2024/10/28 09:46:43 by acennadi         ###   ########.fr       */
+/*   Updated: 2024/10/28 10:08:58 by acennadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,12 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*str;
-	int		i;
-	int		j;
 
 	if (!s1 || !s2)
 		return (NULL);
-	str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (!str)
+	if (!(str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1)))
 		return (NULL);
-	i = 0;
-	while (s1[i] != '\0')
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (s2[j])
-	{
-		str[i] = s2[j];
-		i++;
-		j++;
-	}
-	str[i] = '\0';
+	ft_strlcpy(str, s1, ft_strlen(s1) + ft_strlen(s2) + 1);
+	ft_strlcat(str, s2, ft_strlen(s1) + ft_strlen(s2) + 1);
 	return (str);
 }
