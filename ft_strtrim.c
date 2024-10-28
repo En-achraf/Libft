12d89cfb1261783346d55ba29a40022ece5c3fb9
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2024-10-28 21:01:13 +0100

    finish

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acennadi <acennadi@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 18:54:32 by acennadi          #+#    #+#             */
+/*   Updated: 2024/10/28 21:00:35 by acennadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 static int	its_set(char const s1, char const *s)
@@ -5,24 +17,10 @@ static int	its_set(char const s1, char const *s)
 	while (*s)
 	{
 		if (s1 == *s)
-		{
 			return (1);
-		}
 		s++;
 	}
 	return (0);
-}
-
-static int	len(char const *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		i++;
-	}
-	return (i);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
@@ -33,7 +31,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char	*str;
 
 	start = 0;
-	end = len(s1);
+	if(!s1 || !set)
+		return (NULL);
+	end = ft_strlen(s1);
 	while (s1[start] && its_set(s1[start], set))
 		start++;
 	while (end > start && its_set(s1[end - 1], set))
