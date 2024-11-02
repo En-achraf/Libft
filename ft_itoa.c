1d68571363bf01dec70d4d58df04cd1de136ba86
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2024-11-02 22:44:36 +0100

    finish

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,64 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acennadi <acennadi@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/02 17:53:05 by acennadi          #+#    #+#             */
+/*   Updated: 2024/11/02 18:56:03 by acennadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-static int	numlen(int n)
-{
-	int	count;
+int numlenth(int i) {
+    int count;
 
-	count = 0;
-	if (n == 0)
-	{
-		return (1);
-	}
-	if (n < 0)
-	{
-		n = -n;
-		count++;
-	}
-	while (n > 0)
-	{
-		n /= 10;
-		count++;
-	}
-	return (count);
+    count = 0;
+    if(i == 0)
+        return (1);
+    if(i < 0) {
+        i = -i;
+        count = 1;
+    }
+        while(i) {
+            i /= 10;
+            count++;
+        }
+    return (count);
+}
+char *ft_buffer(char *str,int len,int n) {
+    int sign;
+
+    sign = 0;
+    if(n < 0) {
+        n = -n;
+        sign = 1;
+    }
+    str[len] = '\0';
+    if(n == 0)
+        str[--len] = '0';
+    while(n) {
+        str[--len] = (n % 10) + 48;
+        n /= 10;
+    }
+    if(sign)
+        str[--len] = '-';
+    return (str);
 }
 
-static void	ft_fill(char *str, int len, int sign, int n)
-{
-	if (n < 0)
-	{
-		sign = 1;
-		n = -n;
-	}
-	str[len] = '\0';
-	if (n == 0)
-	{
-		str[0] = '0';
-	}
-	else
-	{
-		while (n > 0)
-		{
-			str[--len] = (n % 10) + '0';
-			n /= 10;
-		}
-	}
-	if (sign)
-	{
-		str[0] = '-';
-	}
-}
+char *ft_itoa(int n) {
+    int len;
+    char *str;
 
-char	*ft_itoa(int n)
-{
-	int		sign;
-	char	*str;
-	int		len;
-
-	len = numlen(n);
-	str = malloc(sizeof(char) * (len + 1));
-	sign = 0;
-	if (!str)
-		return (NULL);
-	ft_fill(str, len, sign, n);
-	return (str);
+    if (n == -2147483648)
+        return (ft_strdup("-2147483648"));
+    len = numlenth(n);
+    str = malloc(sizeof(char) * (len + 1));
+    if(!str)
+        return NULL;
+    return (ft_buffer(str, len, n));
 }
