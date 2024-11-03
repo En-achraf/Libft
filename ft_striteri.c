375cbe61af033f4843fb89c94892211bb36c3540
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2024-11-03 18:07:54 +0100

    finish!

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,13 +1,24 @@
-void	ft_striteri(char *s, void (*f)(unsigned int, char *))
-{
-	unsigned int	i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acennadi <acennadi@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/03 17:55:34 by acennadi          #+#    #+#             */
+/*   Updated: 2024/11/03 18:07:29 by acennadi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-	if (!s || !f)
-		return ;
-	i = 0;
-	while (s[i])
-	{
-		f(i, &s[i]);
-		i++;
-	}
+#include "libft.h"
+
+void ft_striteri(char *s, void (*f)(unsigned int, char *)) {
+    unsigned int i;
+    
+    i = 0;
+    while (s[i])
+    {
+        f(i, &s[i]);
+        i++;
+    }
 }
