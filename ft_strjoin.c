78168048d6201d9dabf7f04630a647697d45f1f6
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2024-11-14 13:22:47 +0100

    !Done

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -19,9 +19,9 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	if (!s1 && !s2)
 		return (NULL);
 	if (!s1)
-		return ((char *)s2);
+		return (ft_strdup(s2));
 	if (!s2)
-		return ((char *)s1);
+		return (ft_strdup(s1));
 	str = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!str)
 		return (NULL);
